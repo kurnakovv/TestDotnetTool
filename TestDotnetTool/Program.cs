b32f7424435acab452564934b2080ae971899f03
kurnakovv <maksk210@gmail.com>
2026-02-05 10:39:54 +0900

    Add release note file

--- TestDotnetTool/Program.cs
+++ TestDotnetTool/Program.cs
@@ -2,3 +2,16 @@
 string bot = $"\nHello from kurnakovv!";
 bot += @"";
 Console.WriteLine(bot);
+//string currentDirectory = Directory.GetCurrentDirectory();
+//string editorConfigPath = Path.Combine(currentDirectory, ".editorconfig");
+
+//Console.WriteLine(editorConfigPath);
+
+//if (File.Exists(editorConfigPath))
+//{
+//    Console.WriteLine($".editorconfig найден: {editorConfigPath}");
+//}
+//else
+//{
+//    Console.WriteLine(".editorconfig не найден в текущей директории.");
+//}
